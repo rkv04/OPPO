6ddeaf46c58700acece259c57b9e103e3d55897c
Kirill Rozhkov <rozhkovkirill04@gmail.com>
2023-09-15 21:14:46 +0700

    Program version 2

--- main.cpp
+++ main.cpp
@@ -2,24 +2,53 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <clocale>
+using namespace std;
 
-struct Sea {
-    std::string title;
+class Sea {
+public:
+    Sea(string sea_title, float sea_depth, float sea_salinity) : title{sea_title}, depth{sea_depth}, salinity{sea_salinity}
+    {
+    }
+    void print(ostream &ost) {
+        ost << this->title << " " << this->depth << " " << this->salinity << endl;
+    }
+private:
+    string title;
     float depth;
     float salinity;
 };
 
+Sea* getSea(std::ifstream &ist) {
+    string title;
+    float depth, salinity;
+    ist >> title >> depth >> salinity;
+    Sea *newSea = new Sea(title, depth, salinity);
+    return newSea;
+}
+
+void printSeas(const vector<Sea*> &seas) {
+    for (auto sea : seas) {
+        sea->print(std::cout);
+    }
+}
+
+void deleteSeas(vector<Sea*> &seas) {
+    for (auto i : seas) {
+        delete i;
+    }
+}
+
 int main() {
-    std::ifstream ist("seas.txt");
-    std::vector<Sea> seas;
+    setlocale(LC_ALL, "rus");
+    vector<Sea*> seas;
+    ifstream ist("seas.txt");
     while (!ist.eof()) {
-        Sea newSea;
-        ist >> newSea.title >> newSea.depth >> newSea.salinity;
+        Sea *newSea = getSea(ist);
         seas.push_back(newSea);
     }
     ist.close();
-    for (int i = 0; i < seas.size(); i++) {
-        std::cout << seas[i].title << " " << seas[i].depth << " " << seas[i].salinity << std::endl;
-    }
+    printSeas(seas);
+    deleteSeas(seas);
     return 0;
 }
